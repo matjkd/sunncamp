39d2ee230df15c61f65004cc3f71118d2a241a74
Mat Sadler <mat@redstudio.co.uk>
2012-05-25 13:53:53 +0000

    added news editor and news page

--- application/models/content_model.php
+++ application/models/content_model.php
@@ -67,6 +67,16 @@ class Content_model extends CI_Model {
             return $query->result();
         }
     }
+    
+    function get_content_cat($cat) {
+    
+    	$this->db->where('category', $cat);
+    	$this->db->order_by('date_added', 'desc');
+    	$query = $this->db->get('content');
+    	if ($query->num_rows > 0) {
+    		return $query->result();
+    	}
+    }
 
     /**
      *
@@ -88,6 +98,21 @@ class Content_model extends CI_Model {
         if ($query->num_rows > 0) {
             return $query->result();
         }
+    }
+    /**
+     *
+     * @param type $filename
+     * @param type $blog_id
+     * @return type
+     */
+    function add_file($filename, $blog_id) {
+    	$content_update = array(
+    			'news_image' => $filename
+    	);
+    
+    	$this->db->where('content_id', $blog_id);
+    	$update = $this->db->update('content', $content_update);
+    	return $update;
     }
 
     function get_all_news() {
@@ -134,23 +159,73 @@ class Content_model extends CI_Model {
         }
     }
 
+   
+    function check_menu_duplicate($menu, $id =0) {
+    	$this->db->where('menu', $menu);
+    	$this->db->where_not_in('content_id', $id);
+    	$query = $this->db->get('content');
+    	if ($query->num_rows > 0)
+    	{
+    		return false;
+    	} else {
+    		return true;
+    	}
+    }
+    
     function add_content() {
-
-        // build array for the model
-        $name = "" . $this->session->userdata('firstname') . " " . $this->session->userdata('lastname') . "";
-        $format = 'DATE_RFC1123';
-        $now = time();
-        $datetime = $now;
-        $form_data = array(
-            'title' => set_value('title'),
-            'content' => $this->input->post('content'),
-            'menu' => $this->input->post('menu'),
-            'category' => set_value('category'),
-            'added_by' => $name,
-            'date_added' => $datetime
-        );
-        $insert = $this->db->insert('content', $form_data);
-        return $insert;
+    
+    	// build array for the model
+    	if($this->input->post('added_by') != NULL) {
+    		$name = $this->input->post('added_by');
+    	} else {
+    		$name = "" . $this->session->userdata('firstname') . " " . $this->session->userdata('lastname') . "";
+    	}
+    		
+    	if($this->input->post('date_added2') != NULL) {
+    		$datetime = $this->input->post('date_added2');
+    	} else {
+    		$now = time();
+    		$datetime = $now;
+    	}
+    
+    	//check menu
+    	$now = time();
+    	$menu_link = $this->input->post('menu');
+    	$search = array(" ");
+    	$replace = "-";
+    
+    	if ($menu_link == NULL) {
+    
+    		$subject = $this->input->post('title');
+    		$menu_link = str_replace($search, $replace, $subject);
+    		 
+    	} else {
+    		$subject = $this->input->post('menu');
+    		$menu_link = str_replace($search, $replace, $subject);
+    	}
+    
+    	$check_menu = $this->check_menu_duplicate($menu_link);
+    
+    	if($check_menu != TRUE) {
+    		$menu = $menu_link.$now;
+    	} else {
+    		$menu = $menu_link;
+    	}
+    	 
+    
+    	 
+    
+    
+    	$form_data = array(
+    			'title' => set_value('title'),
+    			'content' => $this->input->post('content'),
+    			'menu' => $menu,
+    			'category' => set_value('category'),
+    			'added_by' => $name,
+    			'date_added' => $datetime
+    	);
+    	$insert = $this->db->insert('content', $form_data);
+    	return $insert;
     }
 
 }
