692192179d23dfc714b8f692653d51b0a965c73d
Mat Sadler <mat@redstudio.co.uk>
2012-08-13 09:34:47 +0000

    update routes and links

--- application/views/global/sunncamp/social_icons.php
+++ application/views/global/sunncamp/social_icons.php
@@ -33,10 +33,10 @@
 
     <h3 style="color:#ffffff;">Links</h3>
     <p>
-        <a href="#">Sitemap</a><br/>
-        <a href="#">Terms &amp; Conditions</a><br/>
-        <a href="#">Delivery</a><br/>
-        <a href="#">Contact us</a><br/>
+        <a href="<?=base_url()?>sitemap">Sitemap</a><br/>
+        <a href="<?=base_url()?>terms">Terms &amp; Conditions</a><br/>
+        <a href="<?=base_url()?>delivery">Delivery</a><br/>
+        <a href="<?=base_url()?>contact">Contact us</a><br/>
     </p>
 </div>
 
@@ -44,10 +44,10 @@
 
 
     <p>
-        <a href="#">Home</a><br/>
-        <a href="#">News</a><br/>
-        <a href="#">Why Choose Suncamp</a><br/>
-        <a href="#">Stockists</a><br/>
+        <a href="<?=base_url()?>">Home</a><br/>
+        <a href="<?=base_url()?>news">News</a><br/>
+        <a href="<?=base_url()?>about">Why Choose Suncamp</a><br/>
+        <a href="<?=base_url()?>stockists">Stockists</a><br/>
     </p>
 </div>
 
@@ -55,10 +55,10 @@
 
 
     <p>
-        <a href="#">Instructions</a><br/>
-        <a href="#">Testimonials</a><br/>
-        <a href="#">Trade Reviews</a><br/>
-        <a href="#">FAQ</a><br/>
+        <a href="<?=base_url()?>instruction_manuals">Instructions</a><br/>
+        <a href="<?=base_url()?>testimonials">Testimonials</a><br/>
+        <a href="<?=base_url()?>trade_reviews">Trade Reviews</a><br/>
+        <a href="<?=base_url()?>faq">FAQ</a><br/>
     </p>
 </div>
 
