d6c6b30422a5aa584b685b9213ace8c183ae8246
Mat Sadler <mat@redstudio.co.uk>
2012-10-09 16:10:34 +0000

    removed unwanted files

--- images/application/views/extra/trade_reviews.php
+++
@@ -1,6 +0,0 @@
-<?php foreach($extra as $row):?>
-
-
-<a href="<?=base_url()?>images/trade_reviews/<?=$row->trade_review_filename?>"><?=$row->trade_review_title?></a><br/>
-
-<?php endforeach; ?>
--- images/application/views/template/json_features.php
+++
@@ -1,5 +0,0 @@
-[
-<?php foreach($source as $row):?>
-{ "id": "<?=$row['feature_id']?>", "label": "<?=$row['feature_name']?>", "value": "<?=$row['feature_id']?>" }
-<?php endforeach; ?>
-]
