6d6976de8c17a9e7e685881586df053d05489d08
Mat Sadler <mat@redstudio.co.uk>
2012-10-26 12:42:58 +0100

    added brochure images

--- application/views/extra/brochures.php
+++ application/views/extra/brochures.php
@@ -1,12 +1,36 @@
-<div>
-<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SC_Awning_Bro_2012_lowres.pdf">Sunncamp Awning Collection 2012</a>
+<style type="text/css">
+	.brochureLinks {
+		float: left;
+		width:120px;
+		text-align: center;
+		padding: 10px 35px 10px 0px;
+	}
+	.brochureLinks img {
+		width: 120px;
+		height: 120px;
+		-moz-box-shadow: 10px 6px 29px #ddd;
+-webkit-box-shadow: 10px 6px 29px #ddd;
+box-shadow: 10px 6px 29px #ddd;
+	}
+</style>
+
+<div class="brochureLinks">
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SC_Awning_Bro_2012_lowres.pdf"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/awning2012.jpg"/></a>
+	<br/>
+	Sunncamp Awning Collection 2012
 </div>
-<div>
-<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SunnCamp_Accessory_Brochure_2013_low.pdf">Sunncamp Accessories 2013</a>
+<div class="brochureLinks">
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SunnCamp_Accessory_Brochure_2013_low.pdf"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/sunncamp2013.jpg"/></a>
+	<br/>
+	Sunncamp Accessories 2013
 </div>
-<div>
-<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/Sunncamp_2012_tentbro.pdf">Sunncamp Tent Collection 2012</a>
+<div class="brochureLinks">
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/Sunncamp_2012_tentbro.pdf"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/tent2012.jpg"/></a>
+	<br/>
+	Sunncamp Tent Collection 2012
 </div>
-<div>
-<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/TTbro_2012_email.pdf">Sunncamp Trailer Tent Collection 2012</a>
+<div class="brochureLinks">
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/TTbro_2012_email.pdf"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/trailertent2012.jpg"/></a>
+	<br/>
+	Sunncamp Trailer Tent Collection 2012
 </div>
