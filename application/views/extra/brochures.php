c6ec9e2a8ff5d93fa86b9cab9bfe467f4b926bbb
Mat Sadler <mat@redstudio.co.uk>
2013-03-22 22:47:25 +0000

    changed brochures

--- application/views/extra/brochures.php
+++ application/views/extra/brochures.php
@@ -14,23 +14,15 @@ box-shadow: 10px 6px 29px #ddd;
 	}
 </style>
 
+
 <div class="brochureLinks">
-	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SC_Awning_Bro_2012_lowres.pdf"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/awning2012.jpg"/></a>
-	<br/>
-	Sunncamp Awning Collection 2012
-</div>
-<div class="brochureLinks">
-	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SunnCamp_Accessory_Brochure_2013_low.pdf"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/sunncamp2013.jpg"/></a>
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SunnCamp_Accessory_Brochure_2013_low.pdf"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/DSC01239.jpg"/></a>
 	<br/>
 	Sunncamp Accessories 2013
 </div>
+
 <div class="brochureLinks">
-	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/Sunncamp_2012_tentbro.pdf"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/tent2012.jpg"/></a>
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SC_BRO_2013_12+print.pdf"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/DSC01243.jpg"/></a>
 	<br/>
-	Sunncamp Tent Collection 2012
-</div>
-<div class="brochureLinks">
-	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/TTbro_2012_email.pdf"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/trailertent2012.jpg"/></a>
-	<br/>
-	Sunncamp Trailer Tent Collection 2012
+	Tent, Awning, Motor Awning &amp; Trailer Tent Collection 2013
 </div>
