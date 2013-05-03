913df00c4ce269b9caa89c2626d0b4ef66dda946
Mat Sadler <mat@redstudio.co.uk>
2013-05-03 10:33:50 +0100

    minor change to s3 file

--- application/libraries/S3.php
+++ application/libraries/S3.php
@@ -1242,19 +1242,19 @@ class S3 {
 	}
 
 	/**
-	 * Get MIME type for file
-	 *
-	 * @internal Used to get mime types
-	 * @param string &$file File path
-	 * @return string
-	 */
+	* Get MIME type for file
+	*
+	* @internal Used to get mime types
+	* @param string &$file File path
+	* @return string
+	*/
 	public static function __getMimeType(&$file)
 	{
 		$type = false;
 		// Fileinfo documentation says fileinfo_open() will use the
 		// MAGIC env var for the magic file
 		if (extension_loaded('fileinfo') && isset($_ENV['MAGIC']) &&
-				($finfo = finfo_open(FILEINFO_MIME, $_ENV['MAGIC'])) !== false)
+		($finfo = finfo_open(FILEINFO_MIME, $_ENV['MAGIC'])) !== false)
 		{
 			if (($type = finfo_file($finfo, $file)) !== false)
 			{
@@ -1266,28 +1266,26 @@ class S3 {
 			}
 			finfo_close($finfo);
 
-			// If anyone is still using mime_content_type()
-		}
-		elseif (function_exists('mime_content_type'))
+		// If anyone is still using mime_content_type()
+		} elseif (function_exists('mime_content_type'))
 			$type = trim(mime_content_type($file));
 
-		if ($type !== false && strlen($type) > 0)
-			return $type;
+		if ($type !== false && strlen($type) > 0) return $type;
 
 		// Otherwise do it the old fashioned way
 		static $exts = array(
-	'jpg' => 'image/jpeg', 'gif' => 'image/gif', 'png' => 'image/png',
-	'tif' => 'image/tiff', 'tiff' => 'image/tiff', 'ico' => 'image/x-icon',
-	'swf' => 'application/x-shockwave-flash', 'pdf' => 'application/pdf',
-	'zip' => 'application/zip', 'gz' => 'application/x-gzip',
-	'tar' => 'application/x-tar', 'bz' => 'application/x-bzip',
-	'bz2' => 'application/x-bzip2', 'txt' => 'text/plain',
-	'asc' => 'text/plain', 'htm' => 'text/html', 'html' => 'text/html',
-	'css' => 'text/css', 'js' => 'text/javascript',
-	'xml' => 'text/xml', 'xsl' => 'application/xsl+xml',
-	'ogg' => 'application/ogg', 'mp3' => 'audio/mpeg', 'wav' => 'audio/x-wav',
-	'avi' => 'video/x-msvideo', 'mpg' => 'video/mpeg', 'mpeg' => 'video/mpeg',
-	'mov' => 'video/quicktime', 'flv' => 'video/x-flv', 'php' => 'text/x-php'
+			'jpg' => 'image/jpeg', 'gif' => 'image/gif', 'png' => 'image/png',
+			'tif' => 'image/tiff', 'tiff' => 'image/tiff', 'ico' => 'image/x-icon',
+			'swf' => 'application/x-shockwave-flash', 'pdf' => 'application/pdf',
+			'zip' => 'application/zip', 'gz' => 'application/x-gzip',
+			'tar' => 'application/x-tar', 'bz' => 'application/x-bzip',
+			'bz2' => 'application/x-bzip2', 'txt' => 'text/plain',
+			'asc' => 'text/plain', 'htm' => 'text/html', 'html' => 'text/html',
+			'css' => 'text/css', 'js' => 'text/javascript',
+			'xml' => 'text/xml', 'xsl' => 'application/xsl+xml',
+			'ogg' => 'application/ogg', 'mp3' => 'audio/mpeg', 'wav' => 'audio/x-wav',
+			'avi' => 'video/x-msvideo', 'mpg' => 'video/mpeg', 'mpeg' => 'video/mpeg',
+			'mov' => 'video/quicktime', 'flv' => 'video/x-flv', 'php' => 'text/x-php'
 		);
 		$ext = strtolower(pathInfo($file, PATHINFO_EXTENSION));
 		return isset($exts[$ext]) ? $exts[$ext] : 'application/octet-stream';
