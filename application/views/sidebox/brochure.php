fa27aea2aacb513d1a09ed609096bd687049eeca
Mat Sadler <mat@redstudio.co.uk>
2014-11-05 13:54:16 +0000

    icon width

--- application/views/sidebox/brochure.php
+++ application/views/sidebox/brochure.php
@@ -1,3 +1,3 @@
 <?=$this->load->view('sidebox/search')?>
 
-<a href="<?=base_url()?>brochures"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/DSC04524cut.jpg"/></a>
+<a href="<?=base_url()?>brochures"><img style="width:263px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/DSC04524cut.jpg"/></a>
