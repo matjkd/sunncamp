4461bc1b76f837537ff8988cfaeb718420ce3914
Mat Sadler <mat@redstudio.co.uk>
2014-11-05 13:44:23 +0000

    brochure update

--- application/views/extra/brochures.php
+++ application/views/extra/brochures.php
@@ -16,17 +16,17 @@ box-shadow: 10px 6px 29px #ddd;
 
 
 <div class="brochureLinks">
-	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SunnCamp_Accessory_Brochure_2014+FINAL+email.pdf">
-		<img src="https://s3-eu-west-1.amazonaws.com/sunncamp/images/acc_brochure.jpg"/></a>
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/NEC+Oct+2014+Brochure.pdf">
+		<img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/DSC04524cut.jpg"/></a>
 	<br/>
-	Sunncamp Accessories 2014
+	NEC Brochure 2014
 </div>
 
 
 
 <div class="brochureLinks">
-	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/2014scbrochure.pdf">
-		<img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/2014brochure.jpg"/></a>
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/Accessory+Brochure+2015.pdf">
+		<img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/DSC04524cut.jpg"/></a>
 	<br/>
-	Tent, Awning, Motor Awning &amp; Trailer Tent Collection 2014
+	Accessory Brochure 2015
 </div>
--- application/views/sidebox/brochure.php
+++ application/views/sidebox/brochure.php
@@ -1,3 +1,3 @@
 <?=$this->load->view('sidebox/search')?>
 
-<a href="<?=base_url()?>brochures"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/images/brochure_button.jpg"/></a>
+<a href="<?=base_url()?>brochures"><img src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/DSC04524cut.jpg"/></a>
