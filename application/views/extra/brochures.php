8dc89033e30d04188b37e2f168d0aa1c4a537ae9
Mat Sadler <mat@redstudio.co.uk>
2015-02-04 12:28:24 +0000

    add brochure

--- application/views/extra/brochures.php
+++ application/views/extra/brochures.php
@@ -30,3 +30,12 @@ box-shadow: 10px 6px 29px #ddd;
 	<br/>
 	Accessory Brochure 2015
 </div>
+
+
+
+<div class="brochureLinks">
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/TENTBROCHURE2015download.pdf">
+		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/brochure2015.jpg"/></a>
+	<br/>
+	Tent Brochure 2015
+</div>
