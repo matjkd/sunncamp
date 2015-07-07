5c13be35e870d6dcc51f0b67cbc3e21277639b43
Mat Sadler <mat@redstudio.co.uk>
2015-07-07 21:50:24 +0100

    altered slides again

--- application/views/slideshow/slideshow.php
+++ application/views/slideshow/slideshow.php
@@ -1,13 +1,13 @@
 <div id="slideshow">
 
     <div class="frontpage_slideshow">
-  		<img src="<?= base_url() ?>images/slides/Slide_1.jpg"/>
+  		<img src="<?= base_url() ?>images/slides/Slide_1june2015.jpg"/>
 		 <img src="<?= base_url() ?>images/slides/Slide_2.jpg"/>
 		 <!-- <img src="<?= base_url() ?>images/slides/AirVolution_Tents.jpg"/> -->
-		 <img src="<?= base_url() ?>images/slides/Slide_3June2015.jpg"/>
+		 <img src="<?= base_url() ?>images/slides/Slide_3June2015b.jpg"/>
         <img src="<?= base_url() ?>images/slides/Slide_4.jpg"/>
       <img src="<?= base_url() ?>images/slides/Slide_5b.jpg"/>
-      <img src="<?= base_url() ?>images/slides/AirVolution_AwningsJune2015.jpg"/>
+      <img src="<?= base_url() ?>images/slides/AirVolution_AwningsJune2015b.jpg"/>
         
 
     </div>
