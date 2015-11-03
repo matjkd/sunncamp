eb35be54b2d00f009c065c3e89ac4319a6e32e00
Mat Sadler <mat@redstudio.co.uk>
2015-11-03 13:27:03 +0000

    brochure update

--- application/views/extra/brochures.php
+++ application/views/extra/brochures.php
@@ -15,20 +15,15 @@ box-shadow: 10px 6px 29px #ddd;
 </style>
 
 
-<div class="brochureLinks">
-	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/NEC+Oct+2014+Brochure.pdf">
-		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/DSC04525+cut.jpg"/></a>
-	<br/>
-	NEC Brochure 2014
-</div>
+
 
 
 
 <div class="brochureLinks">
-	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/Accessory+Brochure+2015.pdf">
-		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/DSC04524cut.jpg"/></a>
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/Accessory_Brochure_2016.pdf">
+		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/2016accessory.jpg"/></a>
 	<br/>
-	Accessory Brochure 2015
+	Accessory Brochure 2016
 </div>
 
 
