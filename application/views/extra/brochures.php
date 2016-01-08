53a05bcc575c8b0f382e6cc4a354e081c28e8062
Mat Sadler <mat@redstudio.co.uk>
2016-01-08 13:45:21 +0000

    updated tent brochure

--- application/views/extra/brochures.php
+++ application/views/extra/brochures.php
@@ -29,8 +29,8 @@ box-shadow: 10px 6px 29px #ddd;
 
 
 <div class="brochureLinks">
-	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/TENTBROCHURE2015download.pdf">
-		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/brochure2015.jpg"/></a>
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SC+BRO+FINAL.pdf">
+		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/sunncamp2016.png"/></a>
 	<br/>
-	2015 Tent, Awning, Motor Annexe & Utility Tent Brochure
+	2016 Tent, Awning, Motor Annexe & Utility Tent Brochure
 </div>
