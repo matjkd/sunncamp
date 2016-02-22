53f878a0629b4216633c7dde54c78cd4222675cf
Mat Sadler <mat@redstudio.co.uk>
2016-02-22 22:36:11 +0000

    sunnflair to sunncamp

--- application/views/global/sunncamp/disclaimer.php
+++ application/views/global/sunncamp/disclaimer.php
@@ -1,5 +1,5 @@
  <div id="disclaimer">
-                               &copy; Sunnflair Limited. All rights reserved. All images and data on this website are the property 
+                               &copy; SunnCamp Limited. All rights reserved. All images and data on this website are the property 
                                of Sunnflair Ltd. Reproduction of any pictures or data is prohibited without prior written consent of 
-                               Rob Dowse at Sunnflair Head Office 
+                               Rob Dowse at SunnCamp Head Office 
                             </div>
