7b3446f6707b37c0485749eed1736591f4cbef16
Mat Sadler <mat@redstudio.co.uk>
2016-03-21 10:34:27 +0000

    er

--- application/views/sidebox/brochure.php
+++ application/views/sidebox/brochure.php
@@ -1,3 +1,3 @@
 <?=$this->load->view('sidebox/search')?>
 
-<a href="<?=base_url()?>brochures"><img style="width:263px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/brochure2015.jpg"/></a>
+<a href="<?=base_url()?>brochures"><img style="width:263px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/sunncamp2016.png"/></a>
