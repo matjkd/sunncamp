825f61a47d6dbd3d4acafe0b90186f028996fc28
mat sadler <github@redstudio.co.uk>
2017-10-06 10:24:34 +0100

    add another brochure

--- application/views/extra/brochures.php
+++ application/views/extra/brochures.php
@@ -16,6 +16,13 @@ box-shadow: 10px 6px 29px #ddd;
 
 
 <div class="brochureLinks">
+	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SunnCampIconAir2017.pdf">
+		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/brochureIconAir2017.png"/></a>
+	<br/>
+	Icon Air 2017
+</div>
+
+<div class="brochureLinks">
 	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SunnCamp+Brochure.pdf">
 		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/brochure2017.png"/></a>
 	<br/>
