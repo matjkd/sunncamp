111ae6516f4b0ff90a49e1288285cc26a2971666
mat sadler <github@redstudio.co.uk>
2018-01-03 13:30:14 +0000

    update slide

--- application/views/slideshow/slideshow.php
+++ application/views/slideshow/slideshow.php
@@ -2,7 +2,7 @@
 
     <div class="frontpage_slideshow">
   		<img src="<?= base_url() ?>images/slides/Slide_1.jpg"/>
-         <img src="<?= base_url() ?>images/slides/Slide_2b.jpg"/>
+         <img src="<?= base_url() ?>images/slides/2018slide1.png"/>
 		
 		 <!-- <img src="<?= base_url() ?>images/slides/AirVolution_Tents.jpg"/> -->
 		 
