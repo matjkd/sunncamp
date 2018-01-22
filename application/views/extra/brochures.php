2301d8de4c9d00bb1debc84fdd63244f488f59b4
mat sadler <github@redstudio.co.uk>
2018-01-22 09:41:41 +0000

    text change

--- application/views/extra/brochures.php
+++ application/views/extra/brochures.php
@@ -19,7 +19,7 @@ box-shadow: 10px 6px 29px #ddd;
 	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SunnCampIconAir2017.pdf">
 		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/brochureIconAir2017.png"/></a>
 	<br/>
-	Icon Air 2017
+	Icon Air
 </div>
 
 <div class="brochureLinks">
