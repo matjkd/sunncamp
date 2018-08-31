21b6bf5c90ea89a2b0b707a47bade44d3a9f1bc8
mat sadler <github@redstudio.co.uk>
2018-08-31 16:25:25 +0100

    Update brochure.php

--- application/views/sidebox/brochure.php
+++ application/views/sidebox/brochure.php
@@ -1,3 +1,3 @@
 <?=$this->load->view('sidebox/search')?>
 
-<a href="<?=base_url()?>brochures"><img style="width:263px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/brochure2019link.png"/></a>
+<a href="<?=base_url()?>brochures"><img style="width:263px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/2019brochurelink.png"/></a>
