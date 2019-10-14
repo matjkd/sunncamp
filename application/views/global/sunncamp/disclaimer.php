6442331d3e5296e7c9da20e4b4ab9fffccc82eae
mat sadler <github@redstudio.co.uk>
2019-10-14 11:34:25 +0100

    update disclaimer

--- application/views/global/sunncamp/disclaimer.php
+++ application/views/global/sunncamp/disclaimer.php
@@ -1,5 +1,7 @@
  <div id="disclaimer">
                                &copy; SunnCamp Limited. All rights reserved. All images and data on this website are the property 
                                of SunnCamp Ltd. Reproduction of any pictures or data is prohibited without prior written consent of 
-                               Rob Dowse at SunnCamp Head Office 
+                               Rob Dowse at SunnCamp Head Office. The weights, sizes, plans and colours shown on this website are
+                               given as an indication and are approximate. SunnCamp reserve the right to alter specifications as
+                               considered necessary without notice. 
                             </div>
