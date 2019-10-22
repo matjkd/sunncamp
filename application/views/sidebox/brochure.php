772ac4a02a0dc7d88fdb1faf261e1a39784bf792
mat sadler <github@redstudio.co.uk>
2019-10-22 12:36:15 +0100

    Change brochure link on front page

--- application/views/sidebox/brochure.php
+++ application/views/sidebox/brochure.php
@@ -1,3 +1,3 @@
 <?=$this->load->view('sidebox/search')?>
 
-<a href="<?=base_url()?>brochures"><img style="width:263px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/2019brochurelink.png"/></a>
+<a href="<?=base_url()?>brochures"><img style="width:263px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/images/brochurecover.jpg"/></a>
