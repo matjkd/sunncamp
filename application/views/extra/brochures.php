488988fd76d2d3098446dfb92902ea2337d7c36b
mat sadler <github@redstudio.co.uk>
2019-10-22 12:51:04 +0100

    Update date for accessory brochure

--- application/views/extra/brochures.php
+++ application/views/extra/brochures.php
@@ -36,7 +36,7 @@ box-shadow: 10px 6px 29px #ddd;
 	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/2020accessorybrochure.pdf">
 		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/2020accessorybrochure.jpg"/></a>
 	<br/>
-	Accessory Brochure 2019
+	Accessory Brochure 2020
 </div>
 
 
