bd532928dc65adab6949c03e6ddb8afb3ff3fdc2
mat sadler <github@redstudio.co.uk>
2019-12-11 00:45:34 +0000

    Replace images

--- application/views/pages/view_product.php
+++ application/views/pages/view_product.php
@@ -72,19 +72,26 @@
                 $remoteFile = base_url()."images/products/".$row -> product_id."/thumbs/".$row->filename;
                 
                 if(!getimagesize($remoteFile)){
-                 echo "local ";
-                    $fileLocation = "https://s3-eu-west-1.amazonaws.com/".$bucket."/products/".$row -> product_id."/thumbs/".$row->filename;
-                } else {
-                    echo "s3 ";
-                    $fileLocation = $remoteFile;
+                 echo "s3 ";
+                    $fileThumb = "https://s3-eu-west-1.amazonaws.com/".$bucket."/products/".$row -> product_id."/thumbs/".$row->filename;
+                    $fileMedium = "https://s3-eu-west-1.amazonaws.com/".$bucket."/products/".$row -> product_id."/medium/".$row->filename;
+                    $fileLarge = "https://s3-eu-west-1.amazonaws.com/".$bucket."/products/".$row -> product_id."/large/".$row->filename;
+                
+		
+		} else {
+                    echo "local ";
+			$fileThumb = base_url()."images/products/".$row -> product_id."/thumbs/".$row->filename;
+                    $fileMedium = base_url()."images/products/".$row -> product_id."/medium/".$row->filename;
+                    $fileLarge = base_url()."images/products/".$row -> product_id."/large/".$row->filename;
+                
                 }
                
     
                 ?>
 
-                    <a href='https://s3-eu-west-1.amazonaws.com/<?=$bucket ?>/products/<?= $row -> product_id ?>/large/<?= $row -> filename ?>' class='cloud-zoom-gallery' title='Thumbnail 1'
-                       rel="useZoom: 'zoom1', smallImage: 'https://s3-eu-west-1.amazonaws.com/<?=$bucket ?>/products/<?= $row -> product_id ?>/medium/<?= $row -> filename ?>' ">
-                        <img src="https://s3-eu-west-1.amazonaws.com/<?=$bucket ?>/products/<?= $row -> product_id ?>/thumbs/<?= $row -> filename ?>" alt = "Thumbnail 1"/></a>
+                    <a href="<?=$fileLarge?>" class='cloud-zoom-gallery' title='Thumbnail 1'
+                       rel="useZoom: 'zoom1', smallImage: '<?=fileMedium?>' ">
+                        <img src="<?=fileThumb?>" alt = "Thumbnail 1"/></a>
 
 
 
