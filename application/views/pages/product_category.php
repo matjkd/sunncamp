366f945183aa57d798fc2d55cf5879a4de7f824b
mat sadler <github@redstudio.co.uk>
2019-12-11 00:55:32 +0000

    Update product_category.php

--- application/views/pages/product_category.php
+++ application/views/pages/product_category.php
@@ -23,14 +23,35 @@
 
 
 		<?php foreach ($products as $row): ?>
+		        <?php
+                
+                //check if s3 file exists
+                $remoteFile = base_url()."images/products/".$row -> product_id."/thumbs/".$row->filename;
+                
+                if(!getimagesize($remoteFile)){
+                 echo "s3 ";
+                    $fileThumb = "https://s3-eu-west-1.amazonaws.com/".$bucket."/products/".$row -> product_id."/thumbs/".$row->filename;
+                    $fileMedium = "https://s3-eu-west-1.amazonaws.com/".$bucket."/products/".$row -> product_id."/medium/".$row->filename;
+                    $fileLarge = "https://s3-eu-west-1.amazonaws.com/".$bucket."/products/".$row -> product_id."/large/".$row->filename;
+                
 		
+		} else {
+                   echo "local ";
+			$fileThumb = base_url()."images/products/".$row -> product_id."/thumbs/".$row->filename;
+                    $fileMedium = base_url()."images/products/".$row -> product_id."/medium/".$row->filename;
+                    $fileLarge = base_url()."images/products/".$row -> product_id."/large/".$row->filename;
+                
+                }
+               
+    
+                ?>
 
 		<?php if($row->order == 0 ) { ?>
 		<div id="productview">
 			<a href="<?= base_url() ?>products/show/<?= $row->product_id ?>">
 				<div style="border: 0px solid #999999; height: 106px; width: 134px; overflow: hidden; display:table; ">
 					<div style="display:table-cell; vertical-align:middle; text-align: center;">
-					<img  src="https://s3-eu-west-1.amazonaws.com/<?=$bucket?>/products/<?= $row->product_id ?>/thumbs/<?= $row->filename ?>" />
+					<img  src="<?=$fileThumb?>" />
 					
 </div>
 				</div> <?= $row->product_name ?>
