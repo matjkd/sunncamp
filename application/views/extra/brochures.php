ab04431a586814f47ce1d2cdc560e1c4543ac47b
mat sadler <github@redstudio.co.uk>
2020-11-18 17:00:43 +0000

    Update brochures.php

--- application/views/extra/brochures.php
+++ application/views/extra/brochures.php
@@ -20,7 +20,7 @@ box-shadow: 10px 6px 29px #ddd;
 	<a href="https://sunncamp.s3-eu-west-1.amazonaws.com/brochures/SunnCampTradeBro2021.pdf">
 		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/trade2020.jpg"/></a>
 	<br/>
-	Tent and Awning 2021
+	Tent and Awning Brochure 2021
 </div>
 
 
