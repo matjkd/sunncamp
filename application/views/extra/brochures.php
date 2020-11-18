d7317ed7ea33b93b06ad5848302c4081cd9498de
mat sadler <github@redstudio.co.uk>
2020-11-18 10:56:24 +0000

    Update brochures.php

--- application/views/extra/brochures.php
+++ application/views/extra/brochures.php
@@ -17,18 +17,13 @@ box-shadow: 10px 6px 29px #ddd;
 
 
 <div class="brochureLinks">
-	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SunnCamp+Trade+2020.pdf">
+	<a href="https://sunncamp.s3-eu-west-1.amazonaws.com/brochures/SunnCampTradeBro2021.pdf">
 		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/trade2020.jpg"/></a>
 	<br/>
-	Tent and Awning 2020
+	Tent and Awning 2021
 </div>
 
-<div class="brochureLinks">
-	<a href="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/SunnCampTrade2019(29319).pdf">
-		<img style="height:90px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/brochures/2019brochure.png"/></a>
-	<br/>
-	Tent and Awning 2019
-</div>
+
 
 
 
