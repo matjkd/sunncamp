1e79a9ef1ff8bf50fbe960e351eebd3b24e68c4c
mat sadler <github@redstudio.co.uk>
2020-12-01 20:02:14 +0000

    update brochure link on front page

--- application/views/sidebox/brochure.php
+++ application/views/sidebox/brochure.php
@@ -1,3 +1,3 @@
 <?=$this->load->view('sidebox/search')?>
 
-<a href="<?=base_url()?>brochures"><img style="width:263px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/images/brochurecover.jpg"/></a>
+<a href="<?=base_url()?>brochures"><img style="width:263px;" src="https://s3-eu-west-1.amazonaws.com/sunncamp/images/brochurecover21.jpg"/></a>
