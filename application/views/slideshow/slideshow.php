9b69ab165fc35ef6af1513f57b85684881331eec
mat sadler <github@redstudio.co.uk>
2021-05-13 09:12:43 +0100

    Update slideshow.php

--- application/views/slideshow/slideshow.php
+++ application/views/slideshow/slideshow.php
@@ -2,7 +2,7 @@
 
     <div class="frontpage_slideshow">
   		<img src="<?= base_url() ?>images/slides/Slide_1.jpg"/>
-         <img src="<?= base_url() ?>images/slides/2018slide1.png"/>
+         <img src="sunncamp.s3-eu-west-1.amazonaws.com/images/iconair2021.png"/>
 		
 		 <!-- <img src="<?= base_url() ?>images/slides/AirVolution_Tents.jpg"/> -->
 		 
